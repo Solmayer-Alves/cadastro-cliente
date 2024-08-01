0c8213b2e7dc428578a62bbb2dcc9cb08a9d0ea4
Solmayer Alves <solmayer.alves@gmail.com>
2024-08-01 12:37:28 -0300

    feat: criacao e configuracao do projeto cadastro cliente

---
+++ src/main/java/com/cadastro_cliente/api/CadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro_cliente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClienteApplication.class, args);
+	}
+
+}
